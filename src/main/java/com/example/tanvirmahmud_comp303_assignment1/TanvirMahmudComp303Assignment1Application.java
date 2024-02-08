b5f8f9fb635f0fa2fe504552761e6ab53110d2b5
Tanvir Mahmud <tmahmud7@my.centennialcollege.ca>
2024-02-08 00:57:07 -0500

    first commit

---
+++ src/main/java/com/example/tanvirmahmud_comp303_assignment1/TanvirMahmudComp303Assignment1Application.java
@@ -0,0 +1,13 @@
+package com.example.tanvirmahmud_comp303_assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanvirMahmudComp303Assignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TanvirMahmudComp303Assignment1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tanvirmahmud_comp303_assignment1/TanvirMahmudComp303Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tanvirmahmud_comp303_assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanvirMahmudComp303Assignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
